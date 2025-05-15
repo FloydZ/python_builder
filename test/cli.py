e380803e69523b61e9ac75d96c18370c8db3bc85
Floyd Zweydinger <floyd.zweydinger+github@rub.de>
2025-05-15 19:24:57 +0200

    gitbak

---
+++ test/cli.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" tests for the cli interface """
+
+
+def test_simple():
+    """ if this fails something fishy is going on """
+    pass
+
+
+if __name__ == "__main__":
+    test_simple()
